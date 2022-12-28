e64319801eda92d33e44666344830be2b2285bd8
Latrevion <xiaopikaqiu@outlook.com>
2022-12-28 18:38:48 +0800

    修改main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './permission.js'
+import './assets/common/fonts/iconfont.css'
+import store from './store/index.js'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$store = store
 
 new Vue({
   render: h => h(App),
