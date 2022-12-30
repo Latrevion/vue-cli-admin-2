1d2c17cd910a2a360b0c63b5ba03eec0a6ab816e
Latrevion <xiaopikaqiu@outlook.com>
2022-12-30 22:25:04 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,54 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import layout from "@/layout/auto.vue";
 
 Vue.use(VueRouter);
 
-const originalPush = VueRouter.prototype.push;
+const originalPush = VueRouter.prototype.push
 
-VueRouter.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch((err) => err);
-};
+VueRouter.prototype.push = function push (location) {
+
+    return originalPush.call(this, location).catch(err => err)
+
+}
+
 
 
 const routes = [
-  {
-    path: "/",
-    redirect:"/login",
-  },
-  {
-    path:'/login',
-    name:'login',
-    component:()=>import('@/views/login/index.vue')
-  },
-  {
-    path:'/',
-    redirect:'/index',
-    component:()=>import('@/layout/admin.vue'),
-    meta:{title:'admin'},
-    children:[
-        {
-            path:'/FileTransfer',
-            component:layout,
-            children:[
-                path:'index',
-                name:'FileTransfer',
-                component:()=>import('@/views/FileTransfer/index.vue'),
-                meta:{activeMenu:'/FileTransfer/index'}
-            ]
-        }
-    ]
-  }
-];
-
+    {
+        path: '/',
+        redirect: '/login'
+    },
+    {
+        path: '/login',
+        name: "login",
+        component: () =>
+            import('@/views/login/login.vue'),
+    },
+    {
+        path: '/',
+        name: "index",
+        redirect: '/index',
+        component: () => import('@/layout/admin.vue'),
+        meta: { title: 'admin' },
+        children:[
+            {
+                path: '/FileTransfer',
+                component: layout,
+                children: [
+                    {
+                        path: 'index',
+                        name: "文件流转",
+                        component: () => import('@/views/FileTransfer/index.vue'),
+                        meta: { activeMenu: '/FileTransfer/index'  },
+                    },
+                ]
+            },
+        ]
+    },
+]
 export default new VueRouter({
     base: process.env.BASE_URL,
-    routes,
+    routes
 })
