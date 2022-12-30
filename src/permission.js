d417368af6aef55d9f76840c8ed2c7c55bff51e3
Latrevion <xiaopikaqiu@outlook.com>
2022-12-30 22:13:44 +0800

    permission.js更改

--- src/permission.js
+++ src/permission.js
@@ -1,27 +1,30 @@
 import router from './router'
-import { Notification } from 'element-ui'
-import  NProgress  from 'nprogress'
-NProgress.configure({showSpinner:false})
+import { Notification  } from 'element-ui'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+NProgress.configure({ showSpinner: false })
 
-const needLoginRouter =[]
 
-router.beforeEach((to,from,next)=>{
-    const hasToken=localStorage.getItem('token')
-    NProgress.start()
-    if(hasToken){
-        next()
-    }else{
-        if(needLoginRouter.indexOf(to.path)!==-1){
-            next()
-        }else{
-            Notification.warning('请先登录')
-            next('/login?redirect='${to.path})
-            NProgress.done()
-        }
-    }
+
+const needLoginRouter = [];
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  const hasToken = localStorage.getItem('token');
+  if (hasToken) {
     next()
+  } else {
+    if (needLoginRouter.indexOf(to.path) !== 0) {
+      next()
+    } else {
+      Notification.warning('请登录')
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+  next()
 })
 
-router.afterEach((to,from)=>{
-    NProgress.done()
+router.afterEach(() => {
+  NProgress.done()
 })
